806877a8e4ffe9dda910accd71e8a0d998488a7e
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-08-28 14:36:36 +0900

    テーマプロバイダー導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline, createTheme } from "@mui/material";
 import MainRouter from "./MainRouter";
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <MainRouter />
-    </>
+    </ThemeProvider>
   );
 }
 
